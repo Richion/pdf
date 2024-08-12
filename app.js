ac567ad79f36986822348f9051d5e01e9f955ed5
Richion <48645031+Richion@users.noreply.github.com>
2024-08-12 15:38:38 +0800

    pdf09

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'data.html'));
+});
+
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
